ce57eece8e42cfab39363bd3c2451c5014a9e1a7
dcfvg <qwsx@dcfvg.com>
2013-12-15 19:39:52 +0100

    + main script

---
+++ separate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# set -x
+
+assets="assets/"
+scans=$assets"source/"
+result=$assets"result/"
+sikuliIDE="/Applications/SikuliX-IDE.app/Contents/runIDE"
+
+for scan in `find $scans -iname "*.png" -maxdepth 1 -type f`
+do
+	dirname=$(dirname "$scan")
+	filename=$(basename "$scan")
+	extension="${filename##*.}"
+	filenameclean="${filename%.*}"
+	
+	echo $filename
+	open -a Adobe\ Photoshop\ CC.app $scan 
+	$sikuliIDE -r sepa.sikuli
+	
+	mkdir -v $result$filenameclean
+	
+	convert $dirname/$filenameclean.psd $result$filenameclean/$filenameclean-%d.png
+	rm $dirname/$filenameclean.psd
+done
