e91c5f683d651176e4bc52a695837448cb4721d3
dcfvg <qwsx@dcfvg.com>
2014-01-30 10:11:51 +0100

    rename grab scripts

--- tools/grab/posters.sh
+++ tools/grab/posters.sh
@@ -4,6 +4,12 @@ output="$assets/posters"
 
 phantomjs rasterize.js \
 "http://dev.free-idea.dcfvg.com/tools/repartition.php" \
-"$output/map.pdf" \
+"$output/repartition.pdf" \
 A0 1 \
+2>&1 | grep -v "userSpaceScaleFactor"
+
+phantomjs rasterize.js \
+"http://dev.free-idea.dcfvg.com/tools/by-size.php" \
+"$output/by-size.pdf" \
+A0 0.1 \
 2>&1 | grep -v "userSpaceScaleFactor"
