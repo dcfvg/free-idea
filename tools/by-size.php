ec50ddc5fbf06dffd9f782673b16777a3cb67404
dcfvg <qwsx@dcfvg.com>
2014-01-30 10:12:26 +0100

    layout

--- tools/by-size.php
+++ tools/by-size.php
@@ -1,22 +1,25 @@
 <?php
+ini_set('memory_limit', '-1');
+set_time_limit(6000);
+
 $cache = "../assets/cache/";
 include("../function.php");
 
 
 $step = pow(10,$GLOBALS['clusterSize']);
 
-for ($h=0; $h < 300; $h++) {
-    for ($w=0; $w < 300; $w++) {
+for ($h=0; $h < 150; $h++) {
+    for ($w=0; $w < 150; $w++) {
     $s = bzero($w * $step)."x".bzero($h * $step);
     //echo "<li>$s</li>";
     $res = array_merge((array)$res, glob($cache.'/'.$s.'/IMG_*'));
   }
 }
 
-array_splice($res, 30000);
-foreach ($res as $id => $img) {
-  $tab .= '<img src="'.$img.'">';
-}
+$res = array_unique($res);
+//array_splice($res, 35000);
+
+foreach ($res as $id => $img) $tab .= '<img src="'.$img.'">';
 ?>
 <html>
   <head>
@@ -25,9 +28,7 @@ foreach ($res as $id => $img) {
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body id="poster">
-    <p class="size"><?php echo ltrim($size[0],"0")." × ".ltrim($size[1],"0")."px @150dpi ≅ ".pxTocm($size)."" ?></p>
     <?php echo $tab ?>
-    <script src="../js/jquery-1.9.1.js"></script>
   </body>
 </html>
 
