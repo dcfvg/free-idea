2ad678ee36f5d28f93b3e81f46ea2bc6cc5dd46b
dcfvg <qwsx@dcfvg.com>
2014-02-01 22:17:11 +0100

    slow down and use blue detection

--- sepa.sikuli/sepa.py
+++ sepa.sikuli/sepa.py
@@ -2,7 +2,7 @@ Settings.MoveMouseDelay = 0
 workspace = Region(105,145,1219,835)
 layerzone = Region(1409,430,272,620)
 tabszone = Region(40,77,24,22)
-blkpx = Pattern("1390149570064.png").targetOffset(0,-1)
+blkpx = "1390149570064.png"
 
 def findBlackPixel():
     workspace.click(blkpx)
@@ -17,16 +17,21 @@ init()
 
 while 1:
     try:
-        wait(0.5)
         findBlackPixel()
+        wait(0.5)
         type(Key.F2, KeyModifier.CMD)
+        wait(1)
         type(Key.ENTER)
         pass
     
     except FindFailed:
+        wait(1)
+        type("v")
+        click(Location(700,500))
+        wait(1)
         type(Key.F4, KeyModifier.CMD)
-        type(Key.ENTER)
-        type(Key.ENTER)
+        #type(Key.ENTER)
+        #type(Key.ENTER)
         tabszone.waitVanish("1387133617238.png",FOREVER)
         exit()
         pass
