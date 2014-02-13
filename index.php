ac6d0490e20057d028db68b0f5e98d3ffeb8a894
dcfvg <qwsx@dcfvg.com>
2014-02-13 12:22:33 +0100

    update video name

--- index.php
+++ index.php
@@ -18,8 +18,7 @@
   <body id="mainApp">
   	<div id="drawZone"></div>
   	<video id="videoID" width="100%" height="100%">
-    	<source src="capture0002_640x360.mp4" type="video/mp4" />
-    	<track src="capture0002_640x360.srt" kind="subtitle" srclang="en-US" label="English" />
+    	<source src="replay.mov" type="video/mp4" />
     	Your browser does not support HTML5 video.
     </video>
   	<script src="js/jquery-1.9.1.js"></script>
