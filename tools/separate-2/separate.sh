b0a710566554bfa150a09c387b84a329a1682d7e
dcfvg <qwsx@dcfvg.com>
2014-11-28 20:38:06 +0100

    grey scale processing

--- tools/separate-2/separate.sh
+++ tools/separate-2/separate.sh
@@ -14,11 +14,16 @@ scandir="scan/"
 resultdir="result/"
 
 BWbmp="$cache$scanid-bw.bmp"
+greyscale="$cache$scanid-grey.png"
+
 wandRes="$cache$scanid-wandres.png"
 result="$resultdir$scanid-$now.png"
 
 # create bitmap version 
 convert -auto-level -colors 2 +dither -type bilevel $scan $BWbmp  
+
+convert -auto-gamma -auto-level -colorspace Gray -colors 16 $scan $greyscale  
+
 
 # get first black pixel
 position=$(convert "$BWbmp" txt:- | grep "black" | head -n 1)
@@ -27,7 +32,7 @@ IFS=', ' read -a pos <<< "$position"
 wandPos=$((pos[0]))","$((pos[1]+$offset)) # got down 
 
 echo "wand at $wandPos"
-magicwand 1631,626 -t 50 -f image -m transparent -c trans $scan $wandRes
+magicwand 1631,626 -t 60 -f image -m transparent -c trans $greyscale $wandRes
 
 # crop the result
 convert -fuzz 30% -trim $wandRes $result
