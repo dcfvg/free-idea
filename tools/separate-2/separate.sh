d32896e384c84a7fe77d5e530f2cd39f3e5fd49c
dcfvg <qwsx@dcfvg.com>
2014-11-28 20:28:24 +0100

    clean path declaration

--- tools/separate-2/separate.sh
+++ tools/separate-2/separate.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
+# set -x
 
 now=$(date +"%y.%m.%d-%H.%M.%S")
 offset=3
 
+# path definition
+scan=$1
+fscan=$(basename $scan)
+scanid=${fscan%.*}
+
+cache="cache/"
+scandir="scan/"
+resultdir="result/"
+
+BWbmp="$cache$scanid-bw.bmp"
+wandRes="$cache$scanid-wandres.png"
+result="$resultdir$scanid-$now.png"
+
 # create bitmap version 
-convert -auto-level -colors 2 +dither -type bilevel $1 "cache/$(basename $1).bmp"  
+convert -auto-level -colors 2 +dither -type bilevel $scan $BWbmp  
 
 # get first black pixel
-position=$(convert "cache/$(basename$1).bmp" txt:- | grep "black" | head -n 1)
+position=$(convert "$BWbmp" txt:- | grep "black" | head -n 1)
 position="${position%%:*}"
 IFS=', ' read -a pos <<< "$position"
 wandPos=$((pos[0]))","$((pos[1]+$offset)) # got down 
 
 echo "wand at $wandPos"
-magicwand 1631,626 -t 50 -f image -m transparent -c trans $1 "cache/res.png"
+magicwand 1631,626 -t 50 -f image -m transparent -c trans $scan $wandRes
 
 # crop the result
-convert -fuzz 30% -trim "cache/res.png" "result/$now.png"
+convert -fuzz 30% -trim $wandRes $result
