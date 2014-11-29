ef67904ce539cb9187d340b659e10acc5631a857
dcfvg <qwsx@dcfvg.com>
2014-11-29 01:50:38 +0100

    manage with isolated pixels

--- tools/separate-2/separate.sh
+++ tools/separate-2/separate.sh
@@ -3,6 +3,7 @@
 
 now=$(date +"%y.%m.%d-%H.%M.%S")
 offset=3
+margin=200
 
 ext=".mpc"
 # path definition
@@ -19,11 +20,17 @@ bw2bit=$cache"bw.bmp"
 grey=$cache"grey.$ext"
 mask=$cache"mask.$ext"
 imask=$cache"imask.$ext"
+masktrim=$cache"masktrim.$ext"
 
 wandRes=$cache"wandres.$ext"
 result="$resultdir$scanid-$now.png"
 
 i=0
+
+sch=$(identify -format "%[fx:h]" $scan)
+scw=$(identify -format "%[fx:w]" $scan)
+
+echo "scan size : $sch x $scw"
 
 function findAndExtract() {
 	
@@ -35,44 +42,75 @@ function findAndExtract() {
 	result="$resultdir$scanid-$id.png"
 	
 	echo "—--"
-	echo "!!! part #$i"
+	echo "GO! part #$i"
 	echo "[?] looking for a black pixel"
 	
 	position=$(convert "$bw2bit" txt:- | grep "black" | head -n 1)
 	position="${position%%:*}"
 	IFS=', ' read -a pos <<< "$position"
-	wandPos=$((pos[0]))","$((pos[1]+$offset)) # got down 
-
-	#wandPos="1631,626"
-
-	echo " /* wand at $wandPos"
-	magicwand $wandPos -t 50 -f mask -m transparent -c trans -r outside $grey $mask
-
-	echo "->> mask processing"
-	convert $mask -channel rgba \
-		-fill white -opaque none \
-		-transparent black \
-		-fill black -opaque white $imask
-
-	echo "/-/ apply masks"
-	composite -compose copy_opacity $mask $scan $wandRes 		# extract
+	wandPos=$((pos[0]))","$((pos[1]+$offset)) # go down 
+	color=`convert $bw2bit -format "%[pixel:s.p{$wandPos}]" info:` # get the pixel color
 	
-	composite -compose copy_opacity $imask $bw2bit $bw2bit  # delete
-	composite -compose copy_opacity $imask $grey $grey			#
+	echo " /* try magicwand at $wandPos on $color pixel"
 	
-	convert -flatten $bw2bit $bw2bit
+	if [ "$color" == "black" ]
+		then
+			
+			magicwand $wandPos -t 50 -f mask -m transparent -c trans -r outside $grey $mask
+			
+			convert -fuzz 30% -trim $mask $masktrim
+			
+			mh=$(identify -format "%[fx:h]" $masktrim)
+			mw=$(identify -format "%[fx:w]" $masktrim)
+			
+			if (( "$mh" < $((scw-$margin)) )) #[[ $(($mh)) < 3000 ]] # &&  [[ $(($mw)) < $((scw-$margin)) ]]
+				then
+				
+					echo "->> mask processing ($mh x $mw)"
+					convert $mask -channel rgba \
+						-fill white -opaque none \
+						-transparent black \
+						-fill black -opaque white $imask
+
+					echo "-// apply masks"
+					composite -compose copy_opacity $mask $scan $wandRes 		# extract
+
+					composite -compose copy_opacity $imask $bw2bit $bw2bit  # delete
+					composite -compose copy_opacity $imask $grey $grey			#
+
+					convert -background white -alpha remove $bw2bit $bw2bit
+					convert -background white -alpha remove $grey $grey
+			
+					echo "[ ] crop result "
+					convert -fuzz 30% -trim $wandRes $result
+				
+				else
+					echo "!!! mask is too large ! ($mh x $mw)"
+					eraser
+			fi
+		
+		else
+			echo "!!! pixel is $color !"
+			eraser		
+	fi
+	
+	echo "[|] save log image "
 	convert -format jpg $bw2bit $resultdir"/log-$id.jpg"
 
-	echo "[ ] crop result "
-	convert -fuzz 30% -trim $wandRes $result
 }
-
+function eraser(){
+	line="$((pos[0])),$((pos[1])) $wandPos"
+	convert -stroke white -draw "line $line" $bw2bit $bw2bit
+	echo "draw white line @($line)"
+}
 echo "starting $1 conversion"
+
 
 echo "create bitmaps version"
 convert $1 $scan
 convert -auto-gamma -colors 2 +dither -type bilevel $scan $bw2bit  
 convert -auto-gamma -auto-level -colorspace Gray -colors 16 $scan $grey  
+convert -format jpg $bw2bit $resultdir"/log-0000.jpg"
 
 while true; do 
 	findAndExtract
