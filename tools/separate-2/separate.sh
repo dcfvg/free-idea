8d9f518c1b5555d6098a3941885ce56b968a5a8e
dcfvg <qwsx@dcfvg.com>
2015-01-10 12:58:13 +0100

    prevent from looping on the same test

--- tools/separate-2/separate.sh
+++ tools/separate-2/separate.sh
@@ -25,10 +25,15 @@ wandRes=$cache"wandres.$ext"
 result="$resultdir$scanid-$now.png"
 
 i=1
+try=1
+
+wandPosPrev=""
+wandPos=""
+
 function findAndExtract() {
 	
 	echo "    "
-	echo "    part #$i"
+	echo "    part #$i research $try"
 	
 	cropsize=$(convert $bw2bit -fuzz 25% -trim -format '%wx%h%O' info:)
 	
@@ -39,37 +44,50 @@ function findAndExtract() {
 	#echo "[|] save log image "
 	#convert -format jpg $bw2bit $resultdir"/log-$id.jpg"
 	
+	((try++))
+
 	id=`printf %04d $i`
 	result="$resultdir$scanid-$id.png"
 	
-	echo "[?] looking for a black pixel"
+	echo " ?  looking for a black pixel"
 	position=$(convert "$bw2bit" txt:- | LC_ALL=C fgrep  "black" | head -n 1)
 	position="${position%%:*}"
 	IFS=', ' read -a pos <<< "$position"
 	wandPos=$((pos[0]))","$((pos[1])) # go down 
-	
-	echo " /* try magicwand at $wandPos"
-	
+
 	if [ "$wandPos" == "0,0" ]
 		then 
+		echo "  X quit because of bad magicwand position ($wandPos)"
 		exit 0
 	fi
-	
-	if (( "$i" > 500 ))
+
+	if [ "$wandPos" == "$wandPosPrev" ]
+		then 
+			((wandAlert++))
+		else
+			wandAlert=0
+	fi
+	if (( "$wandAlert" > 10 ))
+		then
+		exit 0
+	fi
+	if (( "$try" > 1000 ))
 		then
 		exit 0
 	fi
 
+
+	echo " /* try magicwand at $wandPos"
 	./magicwand $wandPos -t 25 -f mask -m transparent -c trans -r outside $bw2bit $mask
 	convert -fuzz 0% -trim +repage $mask $masktrim
 
-	echo "->> mask processing"
+	echo ">>> mask processing"
 	convert $mask -channel rgba \
 		-fill white -opaque none \
 		-transparent black \
 		-fill black -opaque white $imask
 
-	echo "-// apply masks"
+	echo "/// apply masks"
 	composite -compose copy_opacity $mask $scan $wandRes 	# extract
 	composite -compose copy_opacity $imask $bw2bit $bw2bit  # delete
 	convert -background white -alpha remove $bw2bit $bw2bit #
@@ -79,8 +97,8 @@ function findAndExtract() {
 
 	w=$(identify -format %[fx:w] $wandRes)
 	h=$(identify -format %[fx:h] $wandRes)
-
 	surface=$(( $w * $h ))
+	
 	if (( "$surface" < 100 ))
 		then
 			echo " X  cancel, result surface is too small ($w x $h px : $surface px2 )"
@@ -89,6 +107,8 @@ function findAndExtract() {
 			convert -fuzz 30% -transparent white -resize 99% $wandRes $result
 			((i++))
 	fi
+
+	wandPosPrev=$wandPos
 }
 
 echo "=== starting $1 conversion"
