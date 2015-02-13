9452a5d6142c5764ba43ecbcf09e1c65f879fa62
dcfvg <qwsx@dcfvg.com>
2015-02-14 00:03:39 +0100

    FIFO

--- tools/separate-2/separate-daemon.sh
+++ tools/separate-2/separate-daemon.sh
@@ -7,13 +7,16 @@ prescans="/Users/benoit/Scripts/custom/free-idea/sources/pre-scans"
 scans="/Users/benoit/Scripts/custom/free-idea/sources/scans/"
 
 while true; do
+	
 	detox -r $prescans
+	
 	for file in `find $prescans -iname "*.JP*G"`
 	do
 		mv $file $scans"IMG_"$(stat -f "%m" $file)".jpg"
 	done
 	
 	bash separate-dir.sh $scans
+
 	for (( i=$interval; i>0; i--)); do
 		sleep 1 &
 		printf "next try in $i s \r"
