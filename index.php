7d5d8bbdeb88edb873f21d40cb5a2b2c437df63f
dcfvg <qwsx@dcfvg.com>
2015-11-14 15:27:28 +0100

    cleaning

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
 <?php
-  
   include("function.php");
   session_start();
-  
   if(isset($_GET['f5'])) sortBySize();
   if(isset($_GET['reset'])) unset($_SESSION["history"]);
-  
-  // print_r($_SESSION["history"]);
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>free ideas</title>
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/screen.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/screen.css">
   </head>
   <body id="mainApp">
   	<div id="message" class="presmod">
@@ -26,8 +22,8 @@
     </div>
     <video class="isight" id="my_camera" autoplay="autoplay"></video>
     <div id="drawZone"></div>
-  	<script src="js/jquery-1.9.1.js"></script>
-    <script src="js/jquery-ui.js"></script>
-    <script src="js/free.js"></script>
+  	<script src="assets/js/jquery-1.9.1.js"></script>
+    <script src="assets/js/jquery-ui.js"></script>
+    <script src="assets/js/free.js"></script>
   </body>
 </html>
