d796ca46769956dafbd153f2b0de6bad325a146b
dcfvg <qwsx@dcfvg.com>
2015-12-20 12:09:46 +0100

    get script start path in book render

--- tools/render/bigbook.sh
+++ tools/render/bigbook.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-script=`basename "$0"`
+script=`basename "$0" .sh`
 content="../../app/content"
 cache="$content/cache"
 output="$content/$script"
 
 limit=10000000
 format="13cm*20cm"
-zooms=( 1 0.5 0.6 )
-w_maxs=( 2 8 )
-h_maxs=( 2 50 )
+zooms=( 0.2 0.3 0.1 )
+w_maxs=( 5 10 20 50)
+h_maxs=( 5 20 80 50)
 
 mkdir -p $content $cache $output
 
+echo $script
 printf "\n  format\t zoom\t max w\t max h\t\n"
 printf "\n|-------------------------------------------------|\n"
 
@@ -28,7 +29,7 @@ do
 
       phantomjs rasterize_pdf.js \
       "http://l:8000/views/by-size.php?h_max="$h_max"&w_max="$w_max"&limit="$limit \
-      "$output/book_"$h_max"x"$w_max"_"$zoom"-"$limit".pdf" \
+      "$output/"$script"_"$h_max"x"$w_max"_"$zoom"-"$limit"-"$date".pdf" \
       $format $zoom
 
     done
